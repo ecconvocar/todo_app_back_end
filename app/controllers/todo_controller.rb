30d587bcc02d9651c2e333e1c77729406f301bf9
Emile Convocar <ecconvocar@gmail.com>
2017-06-17 21:53:37 +0000

    Add conditional logic if statements to the show action of the Todo controller

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,9 +1,23 @@
 class TodoController < ApplicationController
-  def index
-  end
-  
   def show
-    @todo_description = "Make the tasks list"
-    @todo_pomodoro_estimate = 5
+    todo_id = params[:id]
+    
+    if todo_id == '1'
+        @todo_description = "Make the tasks"
+        @todo_pomodoro_estimate = 5
+
+    elsif todo_id == '2'
+        @todo_description = "Make calendar"
+        @todo_pomodoro_estimate = 2
+    elsif todo_id == '3'
+        @todo_description = "Make shop"
+        @todo_pomodoro_estimate = 3
+    elsif todo_id == '4'
+        @todo_description = "Make community"
+        @todo_pomodoro_estimate = 4
+    elsif todo_id == '5'
+        @todo_description = "Make account"
+        @todo_pomodoro_estimate = 4
+    end
   end
 end
