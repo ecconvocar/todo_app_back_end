489fb9a061e0d842154aee93849bbdd2bd8ab423
Emile Convocar <ecconvocar@gmail.com>
2017-06-18 20:31:23 +0000

    Add the RCAV flows for the new and create actions

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,23 +1,16 @@
 class TodoController < ApplicationController
   def show
-    todo_id = params[:id]
-    
-    if todo_id == '1'
-        @todo_description = "Make the tasks"
-        @todo_pomodoro_estimate = 5
-
-    elsif todo_id == '2'
-        @todo_description = "Make calendar"
-        @todo_pomodoro_estimate = 2
-    elsif todo_id == '3'
-        @todo_description = "Make shop"
-        @todo_pomodoro_estimate = 3
-    elsif todo_id == '4'
-        @todo_description = "Make community"
-        @todo_pomodoro_estimate = 4
-    elsif todo_id == '5'
-        @todo_description = "Make account"
-        @todo_pomodoro_estimate = 4
-    end
+    @todo = Todo.find_by_id(params["id"])
+  end
+  
+  def new
+  end
+  
+  def create
+    t = Todo.new
+    t.description = params['description']
+    t.pomodoro_estimate = params['pomodoro_estimate']
+    t.save
+    redirect_to "/todo/show/#{ t.id }"
   end
 end
